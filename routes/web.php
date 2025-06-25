a7892a1c13979463b2be6673610ccc66681aa10c
Omar Lahjouji <or.lahjouji@gmail.com>
2025-06-25 21:03:49 +0300

    Enhance RH dashboard: collaborator detail, edit & delete flow

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,29 @@
 <?php
 
 use App\Http\Controllers\ResourceController;
+use App\Http\Controllers\RH\CollaborateurController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('login');
 });
 
-Route::get('/RH/seeAllresources ', [ResourceController::class, 'showpage']);
+Route::get('/RH/seeAllresources', [ResourceController::class, 'showpage']);
+
 Route::resource('ResourceController', ResourceController::class);
+
+Route::get('/RH/collaborateurs', [CollaborateurController::class, 'dashboard'])
+    ->name('collaborateurs.index');
+
+Route::get('/RH/collaborateurs/{id}', [CollaborateurController::class, 'show'])->name('collaborateurs.show');
+
+// Edit (formulaire)
+Route::get('/RH/collaborateurs/{id}/edit', [CollaborateurController::class, 'edit'])->name('collaborateurs.edit');
+
+// Update (soumission du formulaire)
+Route::put('/RH/collaborateurs/{id}', [CollaborateurController::class, 'update'])->name('collaborateurs.update');
+
+// Delete (suppression)
+Route::delete('/RH/collaborateurs/{id}', [CollaborateurController::class, 'destroy'])->name('collaborateurs.destroy');
 
