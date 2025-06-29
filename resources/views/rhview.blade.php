24cf6f526fa0a4daacc5577cba29d1f9b046991f
zakariaayl <ayougilzaki@gmail.com>
2025-06-29 16:12:20 +0200

    adding logic to plan a maintainance

--- resources/views/rhview.blade.php
+++ resources/views/rhview.blade.php
@@ -64,6 +64,12 @@
                         </label>
                         <input type="text" class="w-full px-3 py-2 border border-black rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" name="utilisateur_affecte">
                         </div>
+                        <div class="m-auto">
+                        <label for="etat" class="block text-sm font-medium text-gray-700 mb-2">
+                            designation
+                        </label>
+                        <input type="text" class="w-full px-3 py-2 border border-black rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" name="designation">
+                        </div>
 
                             <button type="submit" class="bg-green-600 hover:bg-green-300 text-center w-1/12 h-2/3 text-white " >filter</button>
 
